757da532be79403f13c79ebb1c1968ae104fdd79
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-05-19 13:52:35 -0400

    Created Bets App + registered in Admin file + migrated to my SQL

---
+++ backend/bets/models.py
@@ -0,0 +1,9 @@
+from tkinter import CASCADE
+from django.db import models
+from authentication.models import User
+
+class Bets(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pick = models.CharField(max_length=150)
+    amount_bet = models.CharField(max_length=200)
+    won = models.BooleanField(default=False)

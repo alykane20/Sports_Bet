6976d22aa0b90eb3cfa6e1d715acf6f2551bca84
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-05-20 12:12:04 -0400

    Created PUT request in Games app to update which team wins when game ends, tested in postman

--- backend/games/urls.py
+++ backend/games/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from games import views
 
 urlpatterns = [
-    path('', views.games), 
+    path('', views.games),
+    path('<int:pk>/', views.update_game), 
 ]
--- backend/games/views.py
+++ backend/games/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from .models import Game
 from .serializers import GameSerializer
+from django.shortcuts import get_object_or_404
 
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
@@ -18,3 +19,13 @@ def games(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_game(request, pk):
+     if request.method == 'PUT':
+        game = get_object_or_404(Game, pk=pk)
+        serializer = GameSerializer(game, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
